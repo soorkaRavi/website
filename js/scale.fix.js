6ec8989508c8a704e05aa7b3791c5b6e22d6fe9d
Kin Lane <kinlane@gmail.com>
2020-03-09 13:54:03 -0700

    update template

--- js/scale.fix.js
+++ js/scale.fix.js
@@ -1,27 +1,17 @@
-if (/iP(?:od|hone)/.test(navigator.userAgent)) {
-  // deletable at least, i.e.
-  //  if (window.gestureStart && dontWantIt) {
-  //    document.removeEventListener("gesturestart", gestureStart);
-  //    delete window.gestureStart;
-  //  }
-  this.gestureStart = (function(metas, i){
-    function gestureStart() {
-      // address .length once per loop
-      for (i = metas.length; i--;) {
-        if (metas[i].name == "viewport") {
-          metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-        }
-      }
+var metas = document.getElementsByTagName('meta');
+var i;
+if (navigator.userAgent.match(/iPhone/i)) {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
     }
-    // address .length once per loop
-    for (i = metas.length; i--;) {
-      if (metas[i].name == "viewport") {
-        metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-      }
+  }
+  document.addEventListener("gesturestart", gestureStart, false);
+}
+function gestureStart() {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
     }
-    // I might argue it should be on capture phase here
-    document.addEventListener("gesturestart", gestureStart);
-    // to be able to address it out there
-    return gestureStart;
-  }(document.getElementsByTagName('meta')));
+  }
 }
